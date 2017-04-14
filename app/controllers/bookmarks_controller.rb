3f5e1e7c27dd404719cdb67514c028db4f6c6faa
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-04-15 00:33:56 +0200

    Add create_tags

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -10,4 +10,15 @@ class BookmarksController < ApplicationController
     def bookmark_params
       params.require(:bookmark).permit(:url)
     end
+
+    def create_tags
+      params[:tags].split(/\s|,/).each do |tag_name|
+        if tag = Tag.where(name: tag_name).first
+          @bookmark.tags << tag
+        else
+          tag = Tag.create(name: tag_name)
+          @bookmark.tags << tag
+        end
+      end
+    end
 end
