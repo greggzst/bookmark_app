8844cead3c5e5ac116cc62bab0878bcee9686600
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-04-14 10:30:05 +0200

    Add validation

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,18 @@
+require 'uri'
+
 class Bookmark < ApplicationRecord
   belongs_to :website
   has_many :bookmark_tags
   has_many :tags, through: :bookmark_tags
+  validates :url, presence: true, length: { minimum: 3 }
+  validate :url_valid?
+  validates :url, uniqueness: { case_sensitivity: false }
+
+  private
+    def url_valid?
+      uri = URI.parse(url)
+      unless uri.is_a? URI::HTTP && !uri.host.nil?
+        errors.add(:url, "Given URL is invalid")
+      end
+    end
 end
