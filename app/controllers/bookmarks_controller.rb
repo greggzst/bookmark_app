e6df6cc016dee66f5492b75419dc70e49fcbd71b
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-04-15 10:38:15 +0200

    Add create action and modify split regex

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,13 +6,27 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new
   end
 
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+
+    respond_to do |format|
+      if @bookmark.save
+        create_tags
+        format.html { redirect_to bookmarks_url, notice: 'Bookmark was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+
+  end
+
   private
     def bookmark_params
       params.require(:bookmark).permit(:url)
     end
 
     def create_tags
-      params[:tags].split(/\s|,/).each do |tag_name|
+      params[:user_tags].split(/,\s|\s|,/).each do |tag_name|
         if tag = Tag.where(name: tag_name).first
           @bookmark.tags << tag
         else
