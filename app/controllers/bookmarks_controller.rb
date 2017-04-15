aabe1263b74c22d7044a8c5d6612400b602ec0df
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-04-15 23:11:55 +0200

    Fix redirection url

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -10,7 +10,7 @@ class BookmarksController < ApplicationController
     respond_to do |format|
       if @bookmark.save
         create_tags
-        format.html { redirect_to bookmarks_url, notice: 'Bookmark was successfully created.' }
+        format.html { redirect_to root_url, notice: 'Bookmark was successfully created.' }
       else
         format.html { render :new }
       end
