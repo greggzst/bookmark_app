4ddcaf2f16468694f8cc2499754df49ec5234291
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-04-16 20:46:42 +0200

    Add search model method

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,3 +1,16 @@
 class Website < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
+
+  def self.search(search)
+    search = "%#{search}%"
+    #get all the bookmarks meeting criteria
+    result = Bookmark.includes(:tags).where('title LIKE :s OR url LIKE :s OR tags.name LIKE :s', s:search).references(:tags)
+    #get webiste index and bookmark index and map them in one hash
+    result.map(&:get_website_id_and_id).inject({}) do |acc, hash|
+      acc.merge(hash) do |key, v1, v2|
+        arr = [v1, v2]
+        arr.flatten
+      end
+    end
+  end
 end
