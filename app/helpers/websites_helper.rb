0657ae8180a08f9994a093fcffbd71e333d64c6f
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-04-17 23:43:55 +0200

    Add message if no results

--- app/helpers/websites_helper.rb
+++ app/helpers/websites_helper.rb
@@ -1,26 +1,31 @@
 module WebsitesHelper
   def present_search_results(results)
-    list = "<ul class='search-results'>"
-    results.each do |key, value|
-      website = Website.find(key)
-      list += "<li class='website-#{website.id}'><a href='#' class='result-link'>#{website.url}</a>"
-      bookmarks = website.bookmarks.select do |b|
-        if value.is_a?(Array)
-          value.include?(b.id)
-        else
-          b.id == value
+    if results.present?
+      list = "<ul class='search-results'>"
+      results.each do |key, value|
+        website = Website.find(key)
+        list += "<li class='website-#{website.id}'><a href='#' class='result-link'>#{website.url}</a>"
+        bookmarks = website.bookmarks.select do |b|
+          if value.is_a?(Array)
+            value.include?(b.id)
+          else
+            b.id == value
+          end
         end
-      end
-      list += "<ul class='bookmarks-list'>"
-      bookmarks.each do |b|
-        list += "<li class='bookmark-#{b.id}'>"
-        list += render partial: "bookmarks/bookmark", locals: {bookmark: b}
-        list += "</li>"
+        list += "<ul class='bookmarks-list'>"
+        bookmarks.each do |b|
+          list += "<li class='bookmark-#{b.id}'>"
+          list += render partial: "bookmarks/bookmark", locals: {bookmark: b}
+          list += "</li>"
+        end
+        list += "</ul>"
+        list += "</li>"  
       end
       list += "</ul>"
-      list += "</li>"  
+      list.html_safe
     end
-    list += "</ul>"
-    list.html_safe
-  end
+  else
+    message = "<div class='no-results'>There is no results meeting your criteria!</div>"
+    message.html_safe
+  end  
 end
