c4ea7a744456163f1415752054bd83b0700e2529
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-04-18 01:20:04 +0200

    Add comments

--- app/helpers/websites_helper.rb
+++ app/helpers/websites_helper.rb
@@ -1,10 +1,14 @@
 module WebsitesHelper
   def present_search_results(results)
+    #results is a hash - key website id, value either single bookmark id or array of ids
+    #check if has elements
     if results.present?
+      #build results list
       list = "<ul class='search-results'>"
       results.each do |key, value|
         website = Website.find(key)
         list += "<li class='website-#{website.id}'><a href='#' class='result-link'>#{website.url}</a>"
+        #get bookmarks according to values in results hash
         bookmarks = website.bookmarks.select do |b|
           if value.is_a?(Array)
             value.include?(b.id)
@@ -12,6 +16,7 @@ module WebsitesHelper
             b.id == value
           end
         end
+        #build list for bookmakrs and use bookmark partial to render bookmarks
         list += "<ul class='bookmarks-list'>"
         bookmarks.each do |b|
           list += "<li class='bookmark-#{b.id}'>"
@@ -24,6 +29,7 @@ module WebsitesHelper
       list += "</ul>"
       list.html_safe
     else
+      #if not print message
       message = "<div class='no-results'>There is no results meeting your criteria!</div>"
       message.html_safe
     end    
